814f4c884dbe8267f870f165c6cab68856e92e81
recognitionone <recognitionone@gmail.com>
2019-05-16 23:03:00 +0200

    semi-working second timer

--- controller.js
+++ controller.js
@@ -5,9 +5,20 @@ class MyCounterController {
 	}
 
 	init() {
-		this.view.addStartHandler(() => { this.model.start((text) => { this.view.setupText(text) }) });
+		this.view.addStartHandler (() => { this.model.start((text) => { this.view.setupText(text) }) });
 		this.view.addPauseHandler (() => { this.model.pause() });
 		this.view.addResetHandler (() => { this.model.reset((text) => { this.view.setupText(text) }) });
+
+		this.view.addStartHandlerB (() => { 
+			this.model.start((text) => { this.view.setupText(text) }), 
+			this.model.changeColor((theColor) => { this.view.setupColor(theColor) })
+		});
+
+
+		this.view.addPauseHandlerB (() => { this.model.pause() });
+		this.view.addResetHandlerB (() => { this.model.reset((text) => { this.view.setupText(text) }) });
+
+		// this.view.addColorHandler (() => { this.model.changeColor((theColor) => { this.view.setupColor(theColor) }) });
 	}
 }
 
--- view.js
+++ view.js
@@ -5,6 +5,11 @@ class MyCounterView {
 		this.pauseButton = document.getElementById("pauseButton");
 		this.resetButton = document.getElementById("resetButton");
 
+		this.demoB = document.getElementById("demoB");
+		this.startButtonB = document.getElementById("startButtonB");
+		this.pauseButtonB = document.getElementById("pauseButtonB");
+		this.resetButtonB = document.getElementById("resetButtonB");
+
 	}
 
 	addStartHandler(startHandler) {
@@ -19,8 +24,35 @@ class MyCounterView {
 		this.resetButton.onclick = resetHandler;
 	}
 
+//Timer B Handlers
+
+	//button on click przyjmuje ostatnią przypisaną mu funkcję
+	addStartHandlerB(startHandler, changeColorHandler) {
+		this.startButtonB.onclick = () => { startHandler(), changeColorHandler() }
+	}
+
+	addPauseHandlerB(pauseHandler) {
+		this.pauseButtonB.onclick = pauseHandler;
+	}
+
+	addResetHandlerB(resetHandler) {
+		this.resetButtonB.onclick = resetHandler;
+	}	
+
+//here we change the value shown by the timer
 	setupText(text) {
 		this.demo.innerHTML = text;
+		this.demoB.innerHTML = text;
+	}
+
+
+//here we handle color change in Timer B
+	// addColorHandler(changeColorHandler) {
+	// 	this.startButtonB.onclick = changeColorHandler;
+	// }
+
+	setupColor(theColor) {
+		this.demoB.style.color = theColor;
 	}
 
 }
