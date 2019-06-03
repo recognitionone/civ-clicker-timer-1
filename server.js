2ebb4dad031ccd907d0099eac8e360e68baace95
recognitionone <recognitionone@gmail.com>
2019-06-03 19:57:55 +0200

    wip monday scope-up, startin server communication

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
+const path = require('path');
+
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-// app.use(express.static('dist'));
+app.use(express.static('dist'));
+app.use('/assets-dog', express.static('assets'));
 
-// app.get('/', (req, res) => res.send("hello everyone"))
+
+
+app.get('/test', (req, res) => res.sendFile(path.join(__dirname, './index.html'))) 
+//TODOdoczytać o http get, post, delete, put
 
 // app.get('/',function(req,res){ res.render('dist/bundle.js')});
 
-require('./router/main')(app);
-app.set('views',__dirname + '/views');
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+// require('./router/main')(app);
+// app.set('views',__dirname + '/views');
+// app.set('view engine', 'ejs');
+// app.engine('html', require('ejs').renderFile);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
