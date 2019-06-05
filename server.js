d20c02a6d50b1812b44dcd66ae489d71f8a2fb54
recognitionone <recognitionone@gmail.com>
2019-06-05 13:10:38 +0200

    cleaning

--- server.js
+++ server.js
@@ -13,12 +13,5 @@ app.use('/assets-dog', express.static('assets'));
 app.get('/test', (req, res) => res.sendFile(path.join(__dirname, './index.html'))) 
 //TODOdoczytać o http get, post, delete, put
 
-// app.get('/',function(req,res){ res.render('dist/bundle.js')});
-
-// require('./router/main')(app);
-// app.set('views',__dirname + '/views');
-// app.set('view engine', 'ejs');
-// app.engine('html', require('ejs').renderFile);
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
