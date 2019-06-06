27acc5b2d7c11394dc1bf670f418d9498d0916be
recognitionone <recognitionone@gmail.com>
2019-06-06 15:23:05 +0200

    more cleaning

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express()
 const port = 3000
 
 app.use(express.static('dist'));
-app.use('/assets-dog', express.static('assets'));
+app.use('/assets', express.static('assets'));
 
 
 
-app.get('/test', (req, res) => res.sendFile(path.join(__dirname, './index.html'))) 
+app.get('/root', (req, res) => res.sendFile(path.join(__dirname, './index.html'))) 
 //TODOdoczytać o http get, post, delete, put
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
