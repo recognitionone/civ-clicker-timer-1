81c43bf9e79ab3180b9fe2678b6d3cd4dd6b3885
recognitionone <recognitionone@gmail.com>
2019-06-09 16:19:15 +0200

    WIP, thursday call, server setup

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const path = require('path');
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3003
+console.log(process.env.PORT);
 
 app.use(express.static('dist'));
 app.use('/assets', express.static('assets'));
 
 
 
-app.get('/root', (req, res) => res.sendFile(path.join(__dirname, './index.html'))) 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './index.html'))) 
 //TODOdoczytać o http get, post, delete, put
 
+app.get('/:name', (req, res) => res.send("hello " + req.params.name))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
 
